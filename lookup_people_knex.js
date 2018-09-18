66c840e4d755a9375a5c39bd942f254b4a993c0c
StandardGiraffe <dannyfekete@gmail.com>
2018-09-18 20:37:01 +0000

    Added human-readable listing functionality for hits.

--- lookup_people_knex.js
+++ lookup_people_knex.js
@@ -12,13 +12,24 @@ const knex = require('knex')({
 // Take commandline argument
 const name = process.argv[2];
 
+const printResults = function (array) {
+  let index = 1;
+  array.forEach((i) => {
+    console.log(`- ${index}: ${i.first_name} ${i.last_name}, born ${i.birthdate.toLocaleString().slice(0, -9)}.`);
+    index++;
+  })
+}
+
 knex
   .select()
   .table("famous_people")
+  .where("first_name", "ILIKE", name)
+  .orWhere("last_name", "ILIKE", name)
   .asCallback(function(err, rows) {
     if (err) return console.error(err);
 
-    console.log(rows);
+    console.log(`When searching for "${name}", found ${rows.length} entries:\n`);
+    printResults(rows);
   })
   .asCallback(function(err, rows) {
     knex.destroy();
