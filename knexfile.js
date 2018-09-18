0b58fe4df8f3b95395f59fcc19df22c3bd780f3a
StandardGiraffe <dannyfekete@gmail.com>
2018-09-18 23:49:58 +0000

    Created migration system to track database changes.  Added table, 'milestones', to the famous people database.

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+const settings = require("./settings");
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: settings.hostname,
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'settings.database',
+      user:     'settings.username',
+      password: 'settings.password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
